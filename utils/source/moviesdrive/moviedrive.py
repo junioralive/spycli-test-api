0c91c3a046acaefe6fe6c13852d7025ceae4f7d1
junioralive <thejunioralive@gmail.com>
2024-03-26 10:45:26 +0530

    Domain Fixed

--- utils/source/moviesdrive/moviedrive.py
+++ utils/source/moviesdrive/moviedrive.py
@@ -7,7 +7,7 @@ from playwright.async_api import async_playwright
 
 class MoviesDrive:
     def __init__(self):
-        self.base_url = 'https://moviesdrive.today/'
+        self.base_url = 'https://moviesdrive.space/'
         
     def send_request(self, url):
         try:
